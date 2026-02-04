8a344f8147d22182ff6569b08ea5e821837f328d
Pranav Avva <pranav.avva@gmail.com>
2026-02-04 10:57:42 -0500

    use lower blend
    Signed-off-by: Pranav Avva <pranav.avva@gmail.com>

--- lua/nordic/config.lua
+++ lua/nordic/config.lua
@@ -37,7 +37,7 @@ local defaults = {
         -- Available styles: 'dark', 'light'.
         theme = 'dark',
         -- Blending the cursorline bg with the buffer bg.
-        blend = 0.85,
+        blend = 0.50,
     },
     integrations = {
         dashboard = true,
