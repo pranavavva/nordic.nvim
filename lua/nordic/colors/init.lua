b32624b3e13a9bf1485b51ea1157eb5203b48313
Pranav Avva <pranav.avva@gmail.com>
2026-02-04 11:25:42 -0500

    comment color
    Signed-off-by: Pranav Avva <pranav.avva@gmail.com>

--- lua/nordic/colors/init.lua
+++ lua/nordic/colors/init.lua
@@ -95,7 +95,7 @@ function C.build_palette()
     C.info = C.blue2
 
     -- Misc
-    C.comment = C.gray5
+    C.comment = C.white0
 
     -- Modify the palette after generating colors.
     options.after_palette(C)
